bdf486706d941b6da8b446b3843e5f0b9195b638
Duncan Crawford <duncan@noughtsandones.co.uk>
2016-04-18 06:34:17 +0100

    add MessageHeader

---
+++ app/uk/gov/hmrc/mobilemessages/domain/MessageHeader.scala
@@ -0,0 +1,40 @@
+package uk.gov.hmrc.mobilemessages.domain
+
+import org.joda.time.{DateTime, LocalDate}
+import uk.gov.hmrc.play.controllers.RestFormats
+
+case class MessageHeader(id: String,
+                         subject: String,
+                         validFrom: LocalDate,
+                         readTime: Option[DateTime],
+                         readTimeUrl: String,
+                         sentInError: Boolean)
+
+object MessageHeader {
+
+  import play.api.libs.functional.syntax._
+  import play.api.libs.json.{Json, Reads, _}
+
+  implicit val reads = {
+    implicit val dateTimeForm = RestFormats.dateTimeFormats
+    val read = Json.reads[MessageHeader]
+    val legacyRead: Reads[MessageHeader] = (
+      (__ \ "id").read[String] and
+        (__ \ "subject").read[String] and
+        (__ \ "validFrom").read[LocalDate] and
+        (__ \ "readTime").readNullable[DateTime] and
+        (__ \ "readTimeUrl").read[String] and
+        (__ \ "detail" \ "linkType").read[String] and
+        (__ \ "sentInError").read[Boolean]
+      )(convertFromLegacy _)
+    read orElse legacyRead
+  }
+
+  def convertFromLegacy(id: String,
+                        subject: String,
+                        validFrom: LocalDate,
+                        readTime: Option[DateTime],
+                        readTimeUrl: String,
+                        linkType: String,
+                        sentInError: Boolean) = new MessageHeader(id, subject, validFrom, readTime, readTimeUrl, sentInError)
+}
